69a11b01d24fae5b8c79805244c2716c43cbb34f
riead <mdriead.bd@gmail.com>
2025-12-08 13:11:30 +0600

    Next Js Image Processing Practice

--- src/app/(common)/about/page.tsx
+++ src/app/(common)/about/page.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About Page</h1>
+            <Image 
+            src="https://i.postimg.cc/hGDMGdxg/my-passport-photo.png"
+            width={300}
+            height={300}
+            alt = "My Image"
+            />
         </div>
     );
 };
