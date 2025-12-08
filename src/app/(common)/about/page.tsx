4f634968577b51b3fca0343df33ed7370bd58530
riead <mdriead.bd@gmail.com>
2025-12-08 22:32:53 +0600

    Metadata and Google font practice

--- src/app/(common)/about/page.tsx
+++ src/app/(common)/about/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
-
+import myImg from '../../../asset/my_photo.png'
 const AboutPage = () => {
     return (
         <div>
             <h1>About Page</h1>
             <Image 
-            src="https://i.postimg.cc/hGDMGdxg/my-passport-photo.png"
+            src= {myImg}
             width={300}
             height={300}
             alt = "My Image"
